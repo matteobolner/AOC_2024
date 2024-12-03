10f73e48f9eff796fe66b915422de4f55e372d90
matteobolner <matteo.bolner2@unibo.it>
2024-12-04 00:23:42 +0100

    Completed day 3 part 2

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,8 +1,8 @@
 use regex::Regex;
-use std::{fs, u16};
+use std::fs;
 
-fn find_regex(input_string: String) -> Vec<String> {
-    let re = Regex::new(r"mul\(\d+,\d+\)").unwrap();
+fn find_regex(input_string: String, regex_string: String) -> Vec<String> {
+    let re = Regex::new(&regex_string).unwrap();
     let matches_vec: Vec<String> = re
         .find_iter(&input_string)
         .map(|x| String::from(x.as_str()))
@@ -20,13 +20,28 @@ fn extract_and_multiply(input_string: String) -> u64 {
     multiplication
 }
 
-pub fn complete_day_3(input: String) {
-    let input_path = "input/day3/input.txt";
-    let contents = fs::read_to_string(input_path).expect("Trouble reading file");
-    let matched = find_regex(contents);
+pub fn complete_day_3(input_path: String) {
+    let mut contents = fs::read_to_string(input_path).expect("Trouble reading file");
+    contents = contents.replace("\n", "");
+
+    let matched = find_regex(contents.clone(), String::from(r"mul\(\d+,\d+\)"));
     let mut total_sum = 0;
     for i in matched.iter() {
         total_sum += extract_and_multiply(String::from(i))
     }
-    println!("Total sum: {}", total_sum)
+    println!("Total sum: {}", total_sum);
+    let matched_to_remove = find_regex(contents.clone(), String::from(r"don't\(\).*?do\(\)"));
+    for i in matched_to_remove.iter() {
+        contents = contents.replace(i, "");
+    }
+    let last_dont = find_regex(contents.clone(), String::from(r"don't\(\).*"));
+    for i in last_dont.iter() {
+        contents = contents.replace(i, "");
+    }
+    let matched_trimmed = find_regex(contents, String::from(r"mul\(\d+,\d+\)"));
+    let mut total_sum_trimmed = 0;
+    for i in matched_trimmed.iter() {
+        total_sum_trimmed += extract_and_multiply(String::from(i))
+    }
+    println!("Total sum trimmed: {}", total_sum_trimmed);
 }
