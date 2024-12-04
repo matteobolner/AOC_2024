055d274bd1e49ef9fabf1dd80c0fcf779ca22501
matteobolner <matteo.bolner2@unibo.it>
2024-12-04 19:45:10 +0100

    Started day4 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 mod days {
     pub mod day1;
     pub mod day2;
@@ -17,6 +18,49 @@ fn day3() {
     days::day3::complete_day_3(day_3_input)
 }
 
+//fn get_neighboring_positions(matrix: Vec<Vec<char>>, x: i32, y: i32) {}
+
+fn find_char_in_neighbours(
+    matrix: Vec<Vec<char>>,
+    x: i32,
+    y: i32,
+    seeked_char: char,
+) -> Vec<(i32, i32)> {
+    let matrix_width = matrix[0].len();
+    let matrix_height = matrix.len();
+    let mut found_char_coords: Vec<(i32, i32)> = Vec::new();
+    for x_iter in x - 1..x + 2 {
+        for y_iter in y - 1..y + 2 {
+            if x_iter != x && y_iter != y {
+                continue;
+            }
+
+            if x_iter >= 0
+                && y_iter >= 0
+                && x_iter < matrix_width as i32
+                && y_iter < matrix_height as i32
+            {
+                //println!("{} {}", x_iter, y_iter);
+
+                if matrix[x_iter as usize][y_iter as usize] == seeked_char {
+                    println!("{} {} {} {}", x, y, x_iter, y_iter);
+                    found_char_coords.push((x_iter, y_iter));
+                }
+            }
+        }
+    }
+    found_char_coords
+}
+
 fn main() {
-    day3()
+    let input_path = String::from("input/day4/input.txt");
+    let contents = fs::read_to_string(input_path).expect("Trouble reading file");
+    let mut matrix: Vec<Vec<char>> = Vec::new();
+    for i in contents.lines() {
+        matrix.push(i.chars().collect())
+    }
+    let matrix_width = matrix[0].len();
+    let matrix_height = matrix.len();
+    let found = find_char_in_neighbours(matrix, 1, 0, 'X');
+    println!("{:#?}", found);
 }
